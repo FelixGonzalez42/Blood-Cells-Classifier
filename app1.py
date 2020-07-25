14c1f28250e8e5e840bab5271d38369456f2a178
Fag42 <64332131+Fag42@users.noreply.github.com>
2020-07-24 22:16:51 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -112,8 +112,8 @@ app.layout = html.Div([
             id='title',style={'color':'White'}
         ),
         html.Img(
-            src=app.get_asset_url("logods4all.svg"),
-            # style={'backgroundColor':'Purple'}
+           # src=app.get_asset_url("logods4all.svg"),
+            style={'backgroundColor':'Purple'}
         )
     ],
         className="banner",
