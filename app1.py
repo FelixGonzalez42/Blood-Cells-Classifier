c869821c75ce5cc47db32e3f391378a542b15fe7
santialferez <edwin.alferez@urosario.edu.co>
2020-07-31 08:46:29 -0500

    quitando dbc

--- app1.py
+++ app1.py
@@ -15,7 +15,7 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_auth # Use only as a secure option
 from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
+# import dash_core_components as dcc
 import dash_html_components as html
 import time
 from production import predictor
